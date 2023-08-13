4dc19b5a666d1d4af6ae557db4daf77f7792f905
Alexander Matushin <fixercom@yandex.ru>
2023-08-13 17:49:54 +0300

    feat: LiteList interface, TraineeList empty

---
+++ src/main/java/LiteList.java
@@ -0,0 +1,15 @@
+public interface LiteList<E> {
+
+    void add(E element);
+
+    void add(int index, E element);
+
+    E get(int index);
+
+    boolean remove(E element);
+
+    void clear();
+
+    void sort();
+
+}
---
+++ src/main/java/TraineeList.java
@@ -0,0 +1,32 @@
+public class TraineeList<E> implements LiteList<E>{
+
+    @Override
+    public void add(E element) {
+
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(E element) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
