d30e9156d3c884cef3a3ac43b0bf2d462e5c7a78
Alexander Matushin <fixercom@yandex.ru>
2023-08-13 18:30:57 +0300

    feat: implement add(), get(int index) and size() methods

--- src/main/java/TraineeList.java
+++ src/main/java/TraineeList.java
@@ -1,8 +1,21 @@
-public class TraineeList<E> implements LiteList<E>{
+public class TraineeList<E> implements LiteList<E> {
+
+    private static final int INIT_CAPACITY = 10;
+    private static final double MAGNIFICATION_FACTOR = 1.5;
+    private Object[] internalArray;
+    private int size;
+
+    public TraineeList() {
+        internalArray = new Object[INIT_CAPACITY];
+        size = 0;
+    }
 
     @Override
     public void add(E element) {
-
+        if (internalArray.length == size) {
+            expandInternalArray();
+        }
+        internalArray[size++] = element;
     }
 
     @Override
@@ -11,8 +24,9 @@ public class TraineeList<E> implements LiteList<E>{
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public E get(int index) {
-        return null;
+        return (E) internalArray[index];
     }
 
     @Override
@@ -29,4 +43,17 @@ public class TraineeList<E> implements LiteList<E>{
     public void sort() {
 
     }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private void expandInternalArray() {
+        int length = (int) (internalArray.length * MAGNIFICATION_FACTOR);
+        Object[] expandedArray = new Object[length];
+        System.arraycopy(internalArray, 0, expandedArray, 0, size);
+        internalArray = expandedArray;
+    }
+
 }
