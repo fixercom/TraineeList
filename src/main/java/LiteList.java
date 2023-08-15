d66c5b861c7177d954d432fdcfbb1a4bd7255848
Alexander Matyushin <fixercom@yandex.ru>
2023-08-15 08:53:11 +0300

    style: LiteList javaDoc

--- src/main/java/LiteList.java
+++ src/main/java/LiteList.java
@@ -1,5 +1,25 @@
 import java.util.Comparator;
 
+/**
+ * An ordered collection (also known as a sequence). The user of this
+ * interface has precise control over where in the list each element
+ * is inserted. The user can access elements by their integer index
+ * (position in the list).<p>
+ * <p>
+ * The {@code LiteList} interface provides two methods for adding an
+ * element: one to the end of the list, the other to the corresponding
+ * index.<p>
+ * The {@code LiteList} interface provides a method for getting a list
+ * item by index<p>
+ * The {@code LiteList} interface provides a method for removing an
+ * item from a collection<p>
+ * The {@code LiteList} interface provides methods for completely
+ * clearing the list and getting the size of the list<p>
+ * The {@code LiteList} interface provides a method for sorting the list
+ *
+ * @param <E> the type of elements in this list
+ * @author Alexander Matyushin
+ */
 public interface LiteList<E> {
 
     void add(E element);
